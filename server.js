32ba1aa71d1c25734be8e4096c49f2e219a33eeb
Elisabeth Miller <libbymiller09@gmail.com>
2020-04-24 15:37:22 -0700

    changes port in server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 4200;
 
 app.get('/', (req, res) => {
   res.send('Hello World! How ya doing?');
 });
 
-app.listen(4200, () => {
+app.listen(port, () => {
   console.log('Server listening on port 4200!');
 });
