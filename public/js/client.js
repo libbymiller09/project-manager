6e35e29f8150473a40113d1f5401137e0987a7d2
Elisabeth Miller <libbymiller09@gmail.com>
2020-05-07 15:27:18 -0700

    WIP clientjs file for button clicks,etc with jquery ç

---
+++ public/js/client.js
@@ -0,0 +1,20 @@
+// function deleteBtn() {
+//   const deleteBtn = document.getElementById('deleteBtn');
+//   deleteBtn.addEventListener('click', () => {
+//     event.preventDefault();
+//
+//     const id = $(event.target).parent().attr('id');
+//
+//     $.ajax({
+//       url: '/project/projects' + id,
+//       method: 'DELETE',
+//       success: console.log('Project deleted successfully')
+//     });
+//   });
+// };
+//
+//
+// // on page load
+// $(function() {
+//   deleteBtn();
+// });
